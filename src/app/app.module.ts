71150381f33ba737d417e58aa3d6468a15d6b81a
Eugenia Dallasta <mariaedallasta@gmail.com>
2022-09-12 11:04:25 -0300

    start project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ListPersonasComponent } from './components/list-personas/list-personas.component';
+import { AgregarEditarPersonaComponent } from './components/agregar-editar-persona/agregar-editar-persona.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListPersonasComponent,
+    AgregarEditarPersonaComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
